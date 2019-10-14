1994c6a17533575d810cf501a319eef5e48045bd
dragana96 <38357316+dragana96@users.noreply.github.com>
2019-10-14 13:38:07 +0200

    Project done

---
+++ OS_projekat/BCC/debugger.bat
@@ -0,0 +1,2 @@
+cd C:/Users/OS1/Desktop/sd150485d/Projekat/BCC
+C:\bc31/bin/td.exe Projekat.exe
---
+++ OS_projekat/BCC/runner.bat
@@ -0,0 +1,3 @@
+set PathName="C:\Users\OS1\Desktop\sd150485d\Projekat\BCC"
+cd %PathName%
+Projekat.exe 30 30 30
